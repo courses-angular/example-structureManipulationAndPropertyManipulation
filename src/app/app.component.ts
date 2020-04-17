793ba5d4ac6b7fafc396ee0dfbe28df3f5c752cb
yevgenyl <yevgeny@consist.co.il>
2020-04-17 13:04:28 +0300

    adding users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,13 +18,9 @@ interface IUser {
             <h1 class="display-1">Angular
                 <span class="text-muted">Master Class</span>
             </h1>
-<!--            This ng-container get  'condition' as $implicit of template context-->
-            <ng-container #container></ng-container>
+            <ng-container *ngFor="let user of users"></ng-container>
 
-            <!--            *ngIf create Template -->
-            
-<!--            Use alias (as value) for no duplicates in <ng-container>-->
-            <ng-container *ngIf="condition as value">{{value}}</ng-container>
+           
         </div>
 
 
@@ -32,21 +28,10 @@ interface IUser {
     `,
     styles: []
 })
-export class AppComponent implements AfterViewInit, OnInit {
-    @ViewChild('container', {read: ViewContainerRef}) container: ViewContainerRef
-    @ViewChild(TemplateRef) template: TemplateRef<any>
-    condition: string;
-
-
-    ngOnInit(): void {
-        setTimeout(() => {
-            this.condition = 'Resolved!!'
-        }, 3000)
-    }
-
+export class AppComponent implements AfterViewInit {
+   users = [];
     ngAfterViewInit(): void {
-        console.log(this.template)
-        this.container.createEmbeddedView(this.template);
+
 
     }
 }
